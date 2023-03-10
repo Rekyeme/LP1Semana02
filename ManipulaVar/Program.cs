d4973c1375b27ddac14851fdb7076292d3859b9f
Rekyeme <samuelcarvalho.9966@gmail.com>
2023-03-10 10:43:13 +0000

    Sufixo

---
+++ ManipulaVar/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ManipulaVar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 21;
+            int y = 12;
+
+            Console.WriteLine(x);
+            Console.WriteLine(x++ + y++);
+        }
+    }
+}
